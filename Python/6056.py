210919862fe1fa3ddf14b9c0588580d7f83d2ab8
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-05 22:57:04 +0900

    Create 6056.py

---
+++ Python/6056.py
@@ -0,0 +1,10 @@
+a,b = input().split()
+a = int(a)
+b = int(b)
+if bool(a) == True and bool(b) == False:
+   print("True")
+elif bool(a) == False and bool(b) == True:
+   print("True")
+else :
+   print("False")
+
