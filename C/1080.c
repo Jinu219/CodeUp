dfc17d05927dde37cfc9e295b128cc23f410c36f
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-05 22:25:15 +0900

    Create 1080.c

---
+++ C/1080.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, i = 1;
+	int sum = 0;
+
+	scanf("%d", &n);
+
+	while (1)
+	{
+		sum += i;
+
+		if (n <= sum)
+		{
+			printf("%d\n", i);
+			break;
+		}
+
+		i++;
+	}
+
+	return 0;
+}
