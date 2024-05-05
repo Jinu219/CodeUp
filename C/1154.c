c6e4278c6635681ed325aa4ff6f16c531a332f03
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-05 22:32:43 +0900

    Create 1154.c

---
+++ C/1154.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	if (a < b) printf("%d", b - a);
+	else if (a > b)printf("%d", a - b);
+	else if (a = b)printf("%d", a - b);
+	return 0;
+}
