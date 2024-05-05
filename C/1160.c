77f2435092e89f4545e4011d8c631750e3fbff00
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-05 22:34:05 +0900

    Create 1160.c

---
+++ C/1160.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a;
+	scanf("%d", &a);
+	if (a == 1) printf("oh my god");
+	else if (a == 3) printf("oh my god");
+	else if (a == 5) printf("oh my god");
+	else if (a == 7) printf("oh my god");
+	else printf("enjoy");
+	return 0;
+}
